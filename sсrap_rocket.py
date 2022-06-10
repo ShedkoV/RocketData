8178b39d84ce6966a3f0cd87c83542de45e6572d
Uladzislau Shedko(Foranx team) <v.shedko@foranx.by>
2022-06-10 18:41:02 +0300

    Code refactoring, finish version

--- sсrap_rocket.py
+++ sсrap_rocket.py
@@ -1,37 +1,18 @@
+import json
 from abc import ABC, abstractmethod
+import codecs
+import urllib3
+import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
-import json
-import codecs
-import requests
-import urllib3
 
 
+class ParserFacade(ABC):
 
-
-
-
-class ScrapperFacade(ABC):
-
-    def parse(self):    
-        try:
-            page = self.get_page()
-            raw_data = self.get_raw_data(page=page)
-            data = self.clear_data(raw_data)
-            self.save_data(data)
-            return {"error": False, "data": data}
-        except urllib3.exceptions.NewConnectionError as e:
-            result = {"error": True, "message": e}
-        except urllib3.exceptions.MaxRetryError as e:
-            result = {"error": True, "message": e}
-        except requests.exceptions.ConnectionError as e:
-            result = {"error": True, "message": e}
-        except requests.exceptions.MissingSchema as e:
-            result = {"error": True, "message": e}
-        except TypeError as e:
-            result = {"error": True, "message": e}
-        except FileNotFoundError as e:
-            result = {"error": True, "message": e}        
+    def parse(self):
+        page = self.get_page()
+        raw_data = self.get_raw_data(page=page)
+        return self.clear_data(raw_data)
 
     @abstractmethod
     def get_page(self):
@@ -46,16 +27,41 @@ class ScrapperFacade(ABC):
     def clear_data(self, adverts_dict: dict):
         """Make dict with raw data."""
 
+
+class Scrapper:
+
+    def __init__(self, parser: ParserFacade, path: str):
+        self.parser = parser
+        self.path = path
+
+    def process(self):    
+        try:
+            data = self.parser.parse()
+            self.save_data(data)
+            result =  {'error': False, 'data': data}
+        except urllib3.exceptions.NewConnectionError as e:
+            result = {'error': True, 'message': e}
+        except urllib3.exceptions.MaxRetryError as e:
+            result = {'error': True, 'message': e}
+        except requests.exceptions.ConnectionError as e:
+            result = {'error': True, 'message': e}
+        except requests.exceptions.MissingSchema as e:
+            result = {'error': True, 'message': e}
+        except TypeError as e:
+            result = {'error': True, 'message': e}
+        except FileNotFoundError as e:
+            result = {'error': True, 'message': e}
+        return result
+
     def save_data(self, data):
         with codecs.open(self.path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=2, ensure_ascii=False)
 
 
-class KFCScrapper(ScrapperFacade):
+class KFCScrapper(ParserFacade):
 
-    def __init__(self, url: str, path: str) -> None:
+    def __init__(self, url: str) -> None:
         self.url = url
-        self.path = path
 
     def get_page(self):
         return requests.get(url=self.url).json()
@@ -66,10 +72,10 @@ class KFCScrapper(ScrapperFacade):
     def clear_data(self, kfc_dict: dict):
         return [
             {
-                'address': kfc.get('storePublic').get('contacts').get('streetAddress').get('en', 'ru'),
-                'latlon': kfc.get('storePublic').get('contacts').get('coordinates').get('geometry').get('coordinates'),
-                'name': kfc.get('storePublic').get('contacts').get('coordinates').get('properties').get('name').get('en', 'ru'),
-                'phones': kfc.get('storePublic').get('contacts').get('phoneNumber'),
+                'address': kfc.get('storePublic', {}).get('contacts', {}).get('streetAddress', {}).get('en', 'ru'),
+                'latlon': kfc.get('storePublic', {}).get('contacts', {}).get('coordinates', {}).get('geometry', {}).get('coordinates', {}),
+                'name': kfc.get('storePublic', {}).get('contacts', {}).get('coordinates', {}).get('properties', {}).get('name', {}).get('en', 'ru'),
+                'phones': kfc.get('storePublic', {}).get('contacts', {}).get('phoneNumber', {}),
                 'working_hours': self._get_working_hours(kfc.get('storePublic', {}).get('openingHours', {}).get('regularDaily', []))
             } for kfc in kfc_dict
         ]
@@ -102,16 +108,11 @@ class KFCScrapper(ScrapperFacade):
 
         return result
 
-    # def save_data(self, data):
-    #     with codecs.open(self.path, 'w', encoding='utf-8') as file:
-    #         json.dump(data, file, indent=2, ensure_ascii=False)
 
+class ZikoScrapper(ParserFacade):
 
-class ZikoScrapper(ScrapperFacade):
-
-    def __init__(self, url: str, path: str) -> None:
+    def __init__(self, url: str) -> None:
         self.url = url
-        self.path = path
 
     def get_page(self):
         return requests.get(url=self.url).json()
@@ -122,24 +123,19 @@ class ZikoScrapper(ScrapperFacade):
     def clear_data(self, ziko_raw_data: dict):
         return [
             {
-                'address': data.get('address'),
-                'latlon': [data.get('lat'), data.get('lng')],
-                'name': data.get('title'),
+                'address': data.get('address', {}),
+                'latlon': [data.get('lat', {}), data.get('lng', {})],
+                'name': data.get('title', {}),
                 'phones': 'This information is not found in the API',
-                'working_hours': data.get('mp_pharmacy_hours').replace("<br>", " ")
+                'working_hours': data.get('mp_pharmacy_hours', {}).replace('<br>', ' ')
             } for _, data in ziko_raw_data.items()
         ]
 
-    # def save_data(self, data):
-    #     with codecs.open(self.path, 'w', encoding='utf-8') as file:
-    #         json.dump(data, file, indent=2, ensure_ascii=False)
 
+class MonomahScrapper(ParserFacade):
 
-class MonomahScrapper(ScrapperFacade):
-
-    def __init__(self, url: str, path: str) -> None:
+    def __init__(self, url: str) -> None:
         self.url = url
-        self.path = path
         self.header = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
             "(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
@@ -167,32 +163,35 @@ class MonomahScrapper(ScrapperFacade):
             shop_name = shop_adress[shop_adress.index('(') + 1 : shop_adress.index(')')].replace('ТРЦ', '')
             shop_name += ', ' + city  
 
-            geolocator = Nominatim(user_agent="my_request")
+            geolocator = Nominatim(user_agent='my_request')
             location = geolocator.geocode(shop_name)
 
             return [location.latitude, location.longitude]
 
-        except AttributeError as e:
+        except AttributeError:
             return 'Not info'
             
-        except ValueError as e:
+        except ValueError:
             return 'Not info'
 
-    # def save_data(self: str, data):
-    #     with codecs.open(self.path, 'w', encoding='utf-8') as file:
-    #         json.dump(data, file, indent=2, ensure_ascii=False)
+
+def result_hundler(results: list):
+    for result in results:
+        if result.get('error'):
+            print('There is some error', result.get('message'))   
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    kfc_scrapper = KFCScrapper(url='https://api.kfc.com/api/store/v2/store.get_restaurants?showClosed=true', path='kfs.json')
-    kfc_storage = kfc_scrapper.parse()
-    # save_data(path='kfs_restaurants1.json', data=kfc_storage)
+    results = list()
 
-    ziko_scrapper = ZikoScrapper(url='https://www.ziko.pl/wp-admin/admin-ajax.php?action=get_pharmacies', path='ziko.json')
-    ziko_storage = ziko_scrapper.parse()
-    # save_data(path='ziko.json', data=ziko_storage)
+    kfc_scrapper = Scrapper(KFCScrapper(url='https://api.kfc.com/api/store/v2/store.get_restaurants?showClosed=true'),  path='kfs.json')
+    results.append(kfc_scrapper.process())
 
-    monomax_scrap = MonomahScrapper(url='https://monomax.by/map', path='monomah.json')
-    monomax_storage = monomax_scrap.parse()
-    # save_data(path='monomah.json', data=monomax_storage)
+    ziko_scrapper = Scrapper(ZikoScrapper(url='https://www.ziko.pl/wp-admin/admin-ajax.php?action=get_pharmacies'), path='ziko.json')
+    results.append(ziko_scrapper.process())
+
+    monomax_scrap =  Scrapper(MonomahScrapper(url='https://monomax.by/map'), path='monomah.json')
+    results.append(monomax_scrap.process())
+
+    result_hundler(results)
